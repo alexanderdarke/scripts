243d4fcba302adfbba2d84910c93ca1e97423049
alexanderdarke <129825926+alexanderdarke@users.noreply.github.com>
2023-04-22 23:11:22 +0100

    feat: added pip3-upgrade-packages.sh
    Initial commit of pip3-upgrade-packages.sh which can be used to upgrade
    all packages installed with Pip3.

---
+++ pip3-upgrade-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+PIP3="/usr/local/bin/pip3"
+
+# Loop over each package in the output of 'pip3 list'
+for package in $($PIP3 list | tail -n +3 | cut -d ' ' -f 1)
+do
+    printf "%s\n" $package
+    # Upgrade package and it's dependencies
+    $PIP3 install -U $package
+    printf "\n\n"
+done
